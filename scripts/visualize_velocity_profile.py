cf5d275bd02771051315f329a8da5e440ff805ec
kym <kym93358@gmail.com>
2023-05-03 04:07:40 +0900

    revise visualize_velocity_profile.py

--- scripts/visualize_velocity_profile.py
+++ scripts/visualize_velocity_profile.py
@@ -10,16 +10,26 @@ from teb_local_planner.msg import FeedbackMsg, TrajectoryMsg, TrajectoryPointMsg
 from geometry_msgs.msg import PolygonStamped, Point32
 import numpy as np
 import matplotlib.pyplot as plotter
+from geometry_msgs.msg import Twist
+from rosgraph_msgs.msg import Clock
 
 def feedback_callback(data):
-  global trajectory
+  # global trajectory
+  global vel
+  global ang
+  # if not data.trajectories: # empty
+  #   trajectory = []
+  #   return
+  # trajectory = data.trajectories[data.selected_trajectory_idx].trajectory
 
-  if not data.trajectories: # empty
-    trajectory = []
-    return
-  trajectory = data.trajectories[data.selected_trajectory_idx].trajectory
+  vel = data.linear.x
+  ang = data.angular.z
+
+def clock_callback(data):
   
-  
+  global time
+  time = data.clock.secs
+
 def plot_velocity_profile(fig, ax_v, ax_omega, t, v, omega):
   ax_v.cla()
   ax_v.grid()
@@ -36,31 +46,36 @@ def plot_velocity_profile(fig, ax_v, ax_omega, t, v, omega):
   
 def velocity_plotter():
   global trajectory
+  global vel
+  global ang
+  global time
+  global t,v,omega
   rospy.init_node("visualize_velocity_profile", anonymous=True)
   
-  topic_name = "/robot_0/move_base/TebLocalPlannerROS/teb_feedback"
-  rospy.Subscriber(topic_name, FeedbackMsg, feedback_callback, queue_size = 1) # define feedback topic here!
-
-  rospy.loginfo("Visualizing velocity profile published on '%s'.",topic_name) 
-  rospy.loginfo("Make sure to enable rosparam 'publish_feedback' in the teb_local_planner.")
-
+  topic_name = "/robot_0/cmd_vel"
+  r = rospy.Rate(2) # define rate here
   # two subplots sharing the same t axis
   fig, (ax_v, ax_omega) = plotter.subplots(2, sharex=True)
   plotter.ion()
   plotter.show()
+  while not rospy.is_shutdown():
+    rospy.Subscriber(topic_name, Twist, feedback_callback, queue_size = 1) # define feedback topic here!
+    rospy.Subscriber("/clock", Clock, clock_callback, queue_size = 1)
+    rospy.loginfo("Visualizing velocity profile published on '%s'.",topic_name) 
+    rospy.loginfo("Make sure to enable rosparam 'publish_feedback' in the teb_local_planner.")
+
+    
   
 
-  r = rospy.Rate(2) # define rate here
-  while not rospy.is_shutdown():
+  
+  
     
-    t = []
-    v = []
-    omega = []
     
-    for point in trajectory:
-      t.append(point.time_from_start.to_sec())
-      v.append(point.velocity.linear.x)
-      omega.append(point.velocity.angular.z)
+    
+    
+    t.append(float(time))
+    v.append(vel)
+    omega.append(ang)
           
     plot_velocity_profile(fig, ax_v, ax_omega, np.asarray(t), np.asarray(v), np.asarray(omega))
         
@@ -68,7 +83,14 @@ def velocity_plotter():
 
 if __name__ == '__main__': 
   try:
+
+    t = []
+    v = []
+    omega = []
     trajectory = []
+    vel = 0.0
+    ang = 0.0
+    time = 0
     velocity_plotter()
   except rospy.ROSInterruptException:
     pass
